2c0e4deafedfca94f42410436576131069e96092
Italo Correia <italocdaraujo@gmail.com>
2024-12-10 15:06:54 -0300

    Resolvendo problemas da Build no Vercel

--- src/compenents/MusicCover.tsx
+++ src/compenents/MusicCover.tsx
@@ -10,7 +10,7 @@ const MusicCover = () => (
                 <h1 className={styles.titleAlbum}>PARASITA</h1>
                 <p className={styles.descAlbumDate}>Released October 16, 2023</p>
                 <div className={styles.btnLinkMusic}>
-                <a href="https://youtube.com/playlist?list=PLFWypV3Onhh5HNVEm_8Syq-y9qXjXAuna&si=N2QR4fRdOzA9UBAA" target="_blank">Ouvir</a>
+                <a className={styles.linkListen} href="https://youtube.com/playlist?list=PLFWypV3Onhh5HNVEm_8Syq-y9qXjXAuna&si=N2QR4fRdOzA9UBAA" target="_blank">Ouvir</a>
             </div>
           </div>
         </div>
