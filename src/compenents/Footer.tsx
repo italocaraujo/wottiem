4c08e7c60baa3efc4fe016397f979bdd14577ae4
Italo Correia <italocdaraujo@gmail.com>
2025-02-17 05:27:13 -0300

    fix: year on footer

--- src/compenents/Footer.tsx
+++ src/compenents/Footer.tsx
@@ -2,7 +2,7 @@ import styles from "@/styles/Home.module.css";
 
 const Footer = () => (
     <footer className={styles.footer}>
-        © 2024 ZECA CORREIA - Todos os direitos reservados.
+        © 2025 ZECA CORREIA - Todos os direitos reservados.
     </footer>
 );
   
